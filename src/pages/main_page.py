b075f49cad52e8ccfc502425219ff894d454c035
ftrbnd <giosalas25@gmail.com>
2025-11-20 15:13:16 -0800

    Create interface for uploading txt files

--- src/pages/main_page.py
+++ src/pages/main_page.py
@@ -1,8 +1,17 @@
 import gradio as gr
 
+def generate_questions(value):
+    print("Received file!", type(value))
+    return value
+
+def file_input():
+    return gr.File(file_types=[".txt"],label="Upload a .txt file")
+
+def textbox_output():
+    return gr.Textbox(label="Generated Questions")
+
 with gr.Blocks() as demo:
-    t = gr.Textbox()
-    demo.load(lambda: 'Hi', None, t)
+    f = gr.Interface(fn=generate_questions, inputs=[file_input()], outputs=[textbox_output()], flagging_mode="never")
 
 if __name__ == "__main__":
     demo.launch()
