c3cecbd7e935411b2eb306f4f08bfe51fcad16b4
ftrbnd <giosalas25@gmail.com>
2025-11-25 14:33:44 -0800

    Remove multiple pages

--- src/app.py
+++ src/app.py
@@ -1,21 +1,26 @@
 import gradio as gr
 
-import pages.main_page as main_page, pages.second_page as second_page
+def generate_questions(value):
+    print("Received file!", type(value))
+    return value
 
-def shared_navbar():
-    return gr.Navbar(
-        visible=True,
-        main_page_name="Home",
-        value=[("GitHub", "https://github.com/ftrbnd/quiz-generator")]
-    )
+def file_input():
+    return gr.File(file_types=[".txt"],label="Upload a .txt file")
+
+def textbox_output():
+    return gr.Textbox(label="Generated Questions")
 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
-    navbar = shared_navbar()
-    main_page.demo.render()
 
-with demo.route("Second Page"):
-    navbar = shared_navbar()
-    second_page.demo.render()
+    with gr.Row():
+        with gr.Column():
+            file_input = gr.File(file_types=[".txt"], label="Upload a .txt file")
+            submit_btn = gr.Button("Generate Questions", variant="primary")
+        
+        with gr.Column():
+            output = gr.Textbox(label="Generated Questions")
+    
+    submit_btn.click(fn=generate_questions, inputs=file_input, outputs=output)
 
 if __name__ == "__main__":
     demo.launch()
