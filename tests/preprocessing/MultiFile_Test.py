5dadf12c79c913690cd8f3908a387b7e9e0406cc
Edgar Hernandez <143291407+EdgarHernandez1234@users.noreply.github.com>
2025-12-03 19:10:35 -0800

    Create MultiFile_Test.py
    Signed-off-by: Edgar Hernandez <143291407+EdgarHernandez1234@users.noreply.github.com>

---
+++ tests/preprocessing/MultiFile_Test.py
@@ -0,0 +1,9 @@
+import pytest
+from unittest.mock import patch
+import gradio as gr
+import src import quizzes
+class TestMultFileValidation:
+  def IdentifyFile(self.file):
+    print("File is '{self.quiz}'")
+    return file()
+    
